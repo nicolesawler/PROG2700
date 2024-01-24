459240eac6362cf6efed0787b35d6b014ec190bf
Nicole Sawler <7141267+nicolesawler@users.noreply.github.com>
2024-01-24 14:14:14 +0000

    song melody

---
+++ week2dfc_music/script.js
@@ -0,0 +1,28 @@
+function playNoteWithSleep(note, duration){
+    const melodyOutput = document.getElementById("melodyOutput");
+
+    melodyOutput.innerHTML += `🎵 Play ${note} `;
+    
+    setTimeout(() => {
+        melodyOutput.innerHTML += `💤 Sleep ${duration}s `;
+    },duration * 1000 ); //end timeout
+
+}//close function
+
+// Mallory's Melody
+playNoteWithSleep(72, 0.25);
+playNoteWithSleep(76, 0.25);
+playNoteWithSleep(76, 0.25);
+playNoteWithSleep(72, 0.5);
+playNoteWithSleep(83, 0.25);
+playNoteWithSleep(74, 0.25);
+playNoteWithSleep(83, 0.25);
+playNoteWithSleep(79, 0.25);
+playNoteWithSleep(84, 0); //Last note no sleep
+
+function attachMp3() {
+    const audioPlayer = document.querySelector("audio");
+    audioPlayer.src = "https://sonic-pi.mehackit.org/assets/audio/c-major-melody.mp3";
+}
+
+attachMp3();
